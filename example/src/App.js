d08c4a8a8196b4c79df44a7e3b2ee50df93f4867
Logan Waite <lwaite2010@gmail.com>
2022-08-26 07:26:39 -0400

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'password-verifier'
+import 'password-verifier/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
